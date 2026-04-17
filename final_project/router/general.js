fe63ea82252078d9fd89a07723e7a131417d1ca7
Pratik <pratikjoshi4064@gmail.com>
2026-04-17 18:27:21 +0530

    async axios implementation

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,7 @@
-const axios = require("axios");
+const express = require('express');
+const axios = require('axios');
+let books = require("./booksdb.js");
+const public_users = express.Router();
 
 // Get all books using async
 public_users.get('/', async (req, res) => {
@@ -10,7 +13,7 @@ public_users.get('/', async (req, res) => {
   }
 });
 
-// Get by ISBN
+// Get book by ISBN using async
 public_users.get('/isbn/:isbn', async (req, res) => {
   try {
     const response = await axios.get(`http://localhost:5000/isbn/${req.params.isbn}`);
@@ -20,7 +23,7 @@ public_users.get('/isbn/:isbn', async (req, res) => {
   }
 });
 
-// Get by author
+// Get books by author using async
 public_users.get('/author/:author', async (req, res) => {
   try {
     const response = await axios.get(`http://localhost:5000/author/${req.params.author}`);
@@ -30,7 +33,7 @@ public_users.get('/author/:author', async (req, res) => {
   }
 });
 
-// Get by title
+// Get books by title using async
 public_users.get('/title/:title', async (req, res) => {
   try {
     const response = await axios.get(`http://localhost:5000/title/${req.params.title}`);
@@ -39,3 +42,5 @@ public_users.get('/title/:title', async (req, res) => {
     return res.status(500).json({ message: "Error fetching title" });
   }
 });
+
+module.exports.general = public_users;
